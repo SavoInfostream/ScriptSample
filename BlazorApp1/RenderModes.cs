340b93516f6e295285749ad3d4d588c224af08c9
Savo Pejović <savo.pejovic@infostream.co.me>
2023-12-01 15:00:14 +0100

    Initial commit.

---
+++ BlazorApp1/RenderModes.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp1;
+
+public sealed class RenderModeInteractiveServer : RenderModeAttribute
+{
+    public override IComponentRenderMode Mode => (IComponentRenderMode) RenderMode.InteractiveServer;
+}
